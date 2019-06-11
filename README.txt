34017007cf02b5f4ae3c1f2def5caad64e10d5a8
tremckinley <tmck_12@yahoo.com>
2019-06-11 19:03:22 +0800

    committing to deploy

---
+++ README.txt
@@ -0,0 +1,5 @@
+This is only the beginning!
+      The prompt for this project was provided by Codecademy as an optional additional assignment. It has been lightly refactored to fit my desires for it. This is some of my earliest JavaScript Work.
+
+      
+Feedback is Welcome!
---
+++ resources/js/match-game.js
@@ -0,0 +1,122 @@
+var MatchGame = {};
+
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+$(document).ready(function() {
+  var $game = $('#game');
+  var values = MatchGame.generateCardValues();
+  MatchGame.renderCards(values, $game);
+})
+/*
+  Generates and returns an array of matching card values.
+ */
+
+MatchGame.generateCardValues = function () {
+ var seqValues = [];
+
+  for (var value=1; value<=8; value++) {
+    seqValues.push(value);
+    seqValues.push(value);
+};
+
+  var cardValues = [];
+
+  while (seqValues.length>0) {
+    var randomIndex = Math.floor(Math.random() * seqValues.length);
+    var randomValue = seqValues.splice(randomIndex, 1,)[0];
+    cardValues.push(randomValue);
+  }
+
+  return cardValues;
+};
+
+/*
+  Converts card values to jQuery card objects and adds them to the supplied game
+  object.
+*/
+
+MatchGame.renderCards = function(cardValues, $game) {
+  var colors = [
+    'hsl(25, 85%, 65%)',
+    'hsl(55, 85%, 65%)',
+    'hsl(90, 85%, 65%)',
+    'hsl(160, 85%, 65%)',
+    'hsl(220, 85%, 65%)',
+    'hsl(265, 85%, 65%)',
+    'hsl(310, 85%, 65%)',
+    'hsl(360, 85%, 65%)',
+  ];
+
+  $game.empty();
+  $game.data('flippedCards', []);
+
+  for (var valIndex = 0; valIndex<cardValues.length; valIndex++) {
+    var $cardElement = $('<div class="card col-xs-3"></div>'); 
+    var value = cardValues[valIndex];
+    var color = colors[value-1]
+
+    var data = {
+      value: value,
+      isFlipped: false,
+      color: color
+    };
+
+    $cardElement.data(data)
+
+    $game.append($cardElement)
+  }
+
+  $('.card').click(function(){
+    MatchGame.flipCard($(this), $('#game'));
+  })
+};
+
+/*
+  Flips over a given card and checks to see if two cards are flipped over.
+  Updates styles on flipped cards depending whether they are a match or not.
+ */
+
+MatchGame.flipCard = function($card, $game) {
+  if ($card.data('isFlipped')) {
+    return;
+  } 
+
+  $card.css('background-color', $card.data('color'))
+  .text($card.data('value'))
+  .data('isFlipped', true);
+
+  var flippedCards = $game.data('flippedCards');
+  flippedCards.push($card);
+
+  if (flippedCards.length === 2) {
+    if (flippedCards[0].data('value') === flippedCards[1].data('value')) {
+      var matchCSS = {
+        backgroundColor: 'rgb(153, 153, 153)',
+        color: 'rgb(204, 204, 204)',
+      }
+    flippedCards[0].css(matchCSS);
+    flippedCards[1].css(matchCSS);
+  } else {
+    var card1 = flippedCards[0];
+    var card2 = flippedCards[1];
+
+    window.setTimeout(function(){
+      card1
+      .css('background-color', 'rgb(32, 64, 86)')
+      .text('')
+      .data('isFlipped', false);
+    card2
+      .css('background-color', 'rgb(32, 64, 86)')
+      .text('')
+      .data('isFlipped', false);
+    }, 700)
+  }
+   $game.data('flippedCards', []);
+  }
+  
+
+
+};
